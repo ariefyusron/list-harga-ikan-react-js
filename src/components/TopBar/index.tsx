a4029a634f600b28b84f3151fc3e1a88f39657f9
Arief Yusron <arief.yusron@warungpintar.co>
2021-10-11 19:05:42 +0700

    fix: topbar

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -9,20 +9,27 @@ interface Props {
 }
 
 const Component = ({ type, title }: Props) => (
-  <div className="component-topbar">
-    <div className="wrap-topbar">
-      {type === "home" ? (
-        <img className="logo" src={ICONS.logo} alt="logo" />
-      ) : (
-        <>
-          <button className="button-back">
-            <img className="icon-back" src={ICONS.arrowBack} alt="arrow-back" />
-          </button>
-          <h1 className="title">{title}</h1>
-        </>
-      )}
+  <>
+    <div className="component-topbar">
+      <div className="wrap-topbar">
+        {type === "home" ? (
+          <img className="logo" src={ICONS.logo} alt="logo" />
+        ) : (
+          <>
+            <button className="button-back">
+              <img
+                className="icon-back"
+                src={ICONS.arrowBack}
+                alt="arrow-back"
+              />
+            </button>
+            <h1 className="title">{title}</h1>
+          </>
+        )}
+      </div>
     </div>
-  </div>
+    <div style={{ height: 60 }} />
+  </>
 );
 
 Component.defaultProps = {
